77515ca36cd9280c7f904a6a8188ad2959fe81a2
nehasriva25 <69372224+kozihkode@users.noreply.github.com>
2022-11-12 17:31:55 -0500

    Working on Landing page

--- sprout/src/components/landing.js
+++ sprout/src/components/landing.js
@@ -6,30 +6,18 @@ import { Grid, GridItem } from '@chakra-ui/react';
 export function Landing() {
 
 	return(
-	<Grid
-	templateAreas={`"header header"
-					"nav main"
-					"nav footer"`}
-	gridTemplateRows={'50px 1fr 300px'}
-	gridTemplateColumns={'270px 1fr'}
-	h='600px'
-	gap='1'
-	color='blackAlpha.700'
-	fontWeight='bold'
-	>
-	<GridItem pl='2'  area={'header'}>
-		Welcome to Sprout! 
-	</GridItem>
-	<GridItem pl='2' area={'nav'}>
-		<LandingWidget></LandingWidget> 
-	</GridItem>
-	<GridItem pl='2' bg='green.300' area={'main'}>
-		Main
-	</GridItem>
-	<GridItem pl='2' bg='blue.300' area={'footer'}>
-		Footer
-	</GridItem>
-	</Grid>
+        <Grid
+        h='800px'
+        templateRows='repeat(6, 1fr)'
+        templateColumns='repeat(4, 1fr)'
+        gap={4}
+      >
+        <GridItem rowSpan={4} colSpan={1} ><LandingWidget></LandingWidget></GridItem>
+        <GridItem rowSpan={2} colSpan={1} bg='papayawhip' />
+        <GridItem rowSpan={2} colSpan={1} bg='papayawhip' />
+        <GridItem rowSpan={2} colSpan={1} bg='papayawhip' />
+        <GridItem rowSpan={2} colSpan={3} bg='tomato' />
+      </Grid>
 );
 
 {/* <Grid
--- sprout/src/components/landingwidget.js
+++ sprout/src/components/landingwidget.js
@@ -4,13 +4,11 @@ import logo from './melda.png';
 export function LandingWidget() {
 	const property = {
 		imageAlt: 'melda',
-		title: 'Melda the Almighty',
-		reviewCount: 34,
-
+		title: 'Melda the Almighty'
 	  }
 	
 	  return (
-		<Box maxW='250' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+		<Box maxW='sm' height= "530px" borderWidth='1px' borderRadius='lg' overflow='hidden'>
 		  <Image src={logo} alt={property.imageAlt} />
 		  <Box p='6'>
 			<Box display='flex' alignItems='baseline'>
@@ -37,7 +35,7 @@ export function LandingWidget() {
 	
 			<Box display='flex' mt='2' alignItems='center'>
 			  <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-			  Melda bullies profs for extensions all study term long and successfully saves many students from handing in late assignments.
+			  Melda has successfully saved many students from handing in late assignments.
 
 			  Melda the almighty rules, all hail melda
 			  </Box>
