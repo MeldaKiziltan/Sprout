6d4acbef4c941cd4e3fd4fd27ea79d9dd88088a9
Melda Kiziltan <melkiz2346@gmail.com>
2022-11-12 14:12:30 -0500

    Build

--- sprout/src/App.js
+++ sprout/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.png';
+import logo from './sproutLogo.png';
 import './App.css';
+import { NavBar } from './components/navbar.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          {/* <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+
+        </header>
+        <div className='Tabs'>
+          <NavBar></NavBar>
+        </div>
+      </div>
   );
 }
 
---
+++ sprout/src/components/navbar.js
@@ -0,0 +1,37 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import React from 'react';
+
+export function NavBar() {
+	return (
+		<Tabs isFitted variant='soft-rounded' colorScheme='green'>
+            <TabList>
+                <Tab>Home</Tab>
+                <Tab>Role Models</Tab>
+                <Tab>Wins</Tab>
+                <Tab>Meditation</Tab>
+                <Tab>Discussion</Tab>
+                <Tab>Journal</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel>
+                    <p>Landing Page</p>
+                </TabPanel>
+                <TabPanel>
+                    <p>Role Models go here</p>
+                </TabPanel>
+                <TabPanel>
+                    <p>Wins go here</p>
+                </TabPanel>
+                <TabPanel>
+                    <p>Meditation goes here</p>
+                </TabPanel>
+                <TabPanel>
+                    <p>Blah Blah Blah</p>
+                </TabPanel>
+                <TabPanel>
+                    <p>Write write</p>
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
+	);
+}
